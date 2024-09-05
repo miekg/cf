05ffd93e2dd4c5f7a78771d1d5ce36ac96fbfc43
Miek Gieben <miek@miek.nl>
2024-09-05 09:51:23 +0200

    Miek/cfgroup (#48)
    * Just implement cfgroup too
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * Beter container for groups
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * docs
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * more
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * man
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * more
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * Implement the rest
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * Cfgroup in go
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * more
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * acutally use the constants we have defined now
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    ---------
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ cmd/cfgroup/cfgroup.1.md
@@ -0,0 +1,58 @@
+%%%
+title = "cfgroup 1"
+area = "User Commands"
+workgroup = "CFEngine"
+%%%
+
+cfgroup
+=====
+
+## Name
+
+cfgroup - list or query CFEngine configuration
+
+## Synopsis
+
+cfgroup  *[OPTION]*... *[GROUP]*...
+
+## Description
+
+Cfgroup will list or query CFengine configuration details. Note this is an internal tool that may
+or may not be of use to you.
+
+Cfgroup lists hostnames that are part of specified groups. Data sources are group.cf,
+functionals.cf, promises.cf and schedule.cf. If *GROUP*s are given on the command line the members
+of those groups are printed.
+
+For find those files the following algorithm is used:
+
+* if the cwd is inside a git repository and the basename is called 'cfengine' it will use the files from
+    the current git repository.
+* if the cwd is not in a cfengine git repository **cfgroup** will try /var/cfengine.
+
+Options are:
+
+`-i` *FILE*[,*FILE*]...
+: A single *FILE* or a comma seperated list of *FILE*,*FILE* that should be used as input, typically
+used for testing, but also useful to force cfgroup to parse a specific set of files.
+
+`-l`
+:   print all groups to standard output.
+
+`-r` *HOST*
+:   reverse lookup, show the classes for this specific host.
+
+`-d`
+:   enable debug logging.
+
+## See Also
+
+See the project's README for more details. Development takes place on [GitHub](https://github.com/miekg/cf).
+
+## TODO
+
+Implementing the `-o` and `-n` options from the old cfgroup as well.
+
+## Author
+
+Miek Gieben <miek@miek.nl>.
---
+++ cmd/cfgroup/main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/miekg/cf"
+	"github.com/miekg/cf/internal/parse"
+	"github.com/miekg/cf/internal/rd"
+	"github.com/miekg/cf/internal/token"
+	"go.science.ru.nl/log"
+)
+
+var (
+	flagList    = flag.Bool("l", false, "list all defined groups")
+	flagFiles   = flag.String("i", "", "comma seperated list of files to parse")
+	flagReverse = flag.String("r", "", "show the classes for this specific host")
+	flagDebug   = flag.Bool("d", false, "enable debug logging")
+)
+
+const (
+	Groupcf       = "groups.cf"
+	Promisescf    = "promises.cf"
+	Functionalscf = "functionals.cf"
+)
+
+var Filescf = []string{Groupcf, Promisescf, Functionalscf}
+
+func main() {
+	flag.Parse()
+	var (
+		err    error
+		buffer []byte
+	)
+
+	if *flagDebug {
+		log.D.Set()
+	}
+
+	cffiles := IsGit()
+	log.Debugf("Using %v unless -i is given", cffiles)
+
+	// TODO(miek): do something with stdin?
+
+	var (
+		tree  *rd.Tree
+		debug *rd.DebugTree
+	)
+
+	groups := Groups{}
+	files := strings.Split(*flagFiles, ",")
+
+	for _, f := range files {
+		f = strings.TrimSpace(f)
+		if f == "" {
+			continue
+		}
+		log.Debugf("Parsing %s", f)
+		buffer, err = os.ReadFile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tokens, err := cf.Lex(string(buffer))
+		if err != nil {
+			log.Fatal(err)
+		}
+		if cf.IsNoParse(tokens) {
+			fmt.Printf("%s", buffer)
+			return
+		}
+
+		tree, debug, err = cf.ParseTokens(tokens)
+		if err != nil {
+			log.Fatalf("Can not parse %s: %s", f, err)
+		}
+		if tree == nil && debug == nil {
+			log.Fatalf("Can not parse %s", f)
+		}
+		g1 := List(tree)
+		groups = groups.Merge(g1)
+	}
+
+	if *flagList {
+		Print(os.Stdout, groups.Names())
+		return
+	}
+
+	if *flagReverse != "" {
+		Print(os.Stdout, groups.Search(*flagReverse))
+		return
+	}
+
+	// No options, expect at least a group.
+	Print(os.Stdout, groups.Members(flag.Args()))
+}
+
+/*
+List walks the ast and returns the "members" of each promise. This only works is it's fed cfgroup.cf.
+
+The ast looks like:
+
+	├─ Promise
+	│  ├─ {TokenType(-994) "ClusterMLP"}
+	│  ├─ Constraint
+	│  │  ├─ {KeywordReserved or}
+	│  │  ├─ FatArrow
+	│  │  │  └─ {TokenType(-996) =>}
+	│  │  └─ Rval
+	│  │     └─ List
+	│  │        ├─ Litem
+	│  │        │  └─ {NameFunction mlp01}
+	│  │        ├─ {Punctuation ,}
+	│  │        ├─ Litem
+	│  │        │  └─ {NameFunction mlp02}
+	│  │        ├─ {Punctuation ,}
+
+For each Promise we want to items from the Rval after the FatArrow
+*/
+func List(tree *rd.Tree) Groups {
+	groups := Groups{}
+	// also check top level Class, so we only get "vars".
+	tvf := parse.TreeVisitorFunc(func(tree *rd.Tree, entering bool) parse.WalkStatus {
+		if !entering {
+			return parse.GoToNext
+		}
+		t, ok := tree.Data().(string)
+		if t != token.Promise || !ok {
+			return parse.GoToNext
+		}
+		g := Group{}
+		for i, s1 := range tree.Subtrees {
+			if i == 0 {
+				if t, ok := s1.Data().(token.T); ok {
+					g.Name, _ = strconv.Unquote(t.Value)
+				}
+			}
+			c, ok := s1.Data().(string)
+			if !ok {
+				continue
+			}
+			if c != token.Constraint {
+				continue
+			}
+
+			for _, s2 := range s1.Subtrees {
+				r, ok := s2.Data().(string)
+				if !ok {
+					continue
+				}
+				if r == token.Rval {
+					for _, s3 := range s2.Subtrees {
+						l, ok := s3.Data().(string)
+						if !ok {
+							continue
+						}
+						if l == token.List {
+							for _, s4 := range s3.Subtrees {
+								li, ok := s4.Data().(string)
+								if !ok {
+									continue
+								}
+								if li == token.Litem {
+									// there should be one 1 item here, just grab it
+									s5 := s4.Subtrees[0]
+									if t, ok := s5.Data().(token.T); ok {
+										g.Members = append(g.Members, t.Value)
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			groups[g.Name] = g.Members
+		}
+
+		return parse.GoToNext
+	})
+	parse.Walk(tree, tvf)
+	return groups
+}
+
+// IsGit returns the list of files of interest if the current cwd sits in a cfengine repository.
+func IsGit() []string {
+	paths := make([]string, len(Filescf))
+
+	ctx := context.TODO()
+	cmd := exec.CommandContext(ctx, "git", "rev-parse", "--show-toplevel")
+	cmd.Env = []string{"GIT_CONFIG_GLOBAL=/dev/null", "GIT_CONFIG_SYSTEM=/dev/null"}
+
+	buf, err := cmd.CombinedOutput()
+	if err != nil {
+		// not a git repo
+		for i, f := range Filescf {
+			paths[i] = path.Join("/var/cfengine", f)
+		}
+		return paths
+	}
+	gitrepo := strings.TrimSpace(string(buf))
+	// out should be a single line, that is the path of the git repo, check if the basename is 'cfengine'
+	base := path.Base(gitrepo)
+	prefix := "/var/cfengine"
+	if base == "cfengine" {
+		prefix = gitrepo
+	}
+	for i, f := range Filescf {
+		paths[i] = path.Join(prefix, f)
+	}
+	return paths
+}
